8d4fc34120b30041143dbd4312cf87bbd89165a9
pestos <38012832+pestos@users.noreply.github.com>
2024-06-30 00:31:31 +0200

    Projet avancer

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -82,3 +82,56 @@ exports.bookModifyId = (req, res, next) => {
             res.status(400).json({ error });
         });
 };
+
+
+
+
+exports.bookRatingPost = (req, res, next) => {
+    const bookId = req.params.id;
+    const userId = req.body.userId;
+    const grade = req.body.rating;
+
+    Book.findById(bookId)
+        .then((book) => {
+            if (!book) {
+                return res.status(404).send({ message: "Livre non trouvé" });
+            }
+
+            const existingRating = book.ratings.find(
+                (rating) => rating.userId.toString() === userId
+            );
+
+            if (existingRating) {
+                return res
+                    .status(400)
+                    .json({ message: "Vous avez déjà voté pour ce livre" });
+            }
+
+            // Ajoute un nouveau vote
+            book.ratings.push({ userId, grade });
+
+            // Calculer la nouvelle moyenne des notes
+            const sum = book.ratings.reduce((acc, curr) => acc + curr.grade, 0);
+            const averageRating =
+                book.ratings.length > 0 ? sum / book.ratings.length : 0;
+
+            book.averageRating = averageRating;
+
+            return book.save();
+        })
+        .then((book) => res.status(200).json(book))
+        .catch((error) =>
+            res.status(500).json({
+                message: "Erreur lors de l'enregistrement du vote",
+                error,
+            })
+        );
+};
+
+exports.bookRating = (req, res, next) => {
+    Book.find()
+        .sort({ averageRating: -1 })
+        .limit(3)
+        .then((bestBooks) => res.status(200).json(bestBooks))
+        .catch((error) => res.status(400).json({ error }));
+};
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -6,11 +6,11 @@ const multer = require("../middleware/multer-config");
 
 router.get("/", bookCtrl.booksGet);
 router.post("/", auth, multer, bookCtrl.bookPost);
+router.get("/bestrating", bookCtrl.bookRating);
 router.get("/:id", bookCtrl.bookGetId);
 router.delete("/:id", auth, bookCtrl.bookIdDelete);
 router.put("/:id", auth, multer, bookCtrl.bookModifyId);
-// router.get("/bestrating", bookCtrl.bookRating);
+router.post("/:id/rating", auth, bookCtrl.bookRatingPost);
 
-// router.post("books/:id/rating", auth, bookCtrl.bookIdPost);
 
 module.exports = router;
